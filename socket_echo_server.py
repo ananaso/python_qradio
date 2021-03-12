dbefd119eac50468b23c32fc8ec312d6f805950b
Alden Davidson <adavidson+github@protonmail.ch>
2021-03-12 15:58:47 -0800

    disabled packet sendback, print packet to stdout

--- socket_echo_server.py
+++ socket_echo_server.py
@@ -3,6 +3,7 @@
 
 import socket
 import sys
+import struct
 
 from qradio_protocol_cb import QradioProtocolCb
 
@@ -39,16 +40,36 @@ while True:
         print('connection from', client_address)
 
         # Receive the data in small chunks
-        whole_data = ''.encode('latin-1')
+        whole_data = b''
         cmd_length = -1
+        radiosync = b''
         while len(whole_data) != cmd_length:
+            print(f"whole_data: {len(whole_data)} \ cmd_length: {cmd_length}")
             data = connection.recv(16)
+            if data == b'':
+                break
             print(f"received {data!r}")
             whole_data += data
             if cmd_length == -1 and len(whole_data) >= 8:
-                cmd_length = int.from_bytes(
-                    whole_data[4:8], byteorder='big', signed=False)
+                radiosync = whole_data[:4]
+                if radiosync == qradio.RADIOSYNC:
+                    cmd_length = struct.unpack(">I", whole_data[4:8])[0]
         
+        framesync = whole_data[16:20]
+        if framesync == qradio.FRAMESYNC:
+            cmd_counter = struct.unpack(">I", whole_data[8:12])[0]
+            cmd_data_bit_length = struct.unpack(">I", whole_data[12:16])[0]
+            data = whole_data[20:]
+            print("Unfurled received packet:")
+            print(f"\tRADIOSYNC: 0x{radiosync.hex()}")
+            print(f"\tcmd_length: {cmd_length}")
+            print(f"\tcmd_counter: {cmd_counter}")
+            print(f"\tcmd_data_bit_length: {cmd_data_bit_length}")
+            print(f"\tFRAMESYNC: 0x{framesync.hex()}")
+            print(f"\tdata: {data}")
+        
+        """
+        REMOVED SENDBACK SINCE IT ISN'T WHAT COSMOS EXPECTS/WANTS
         # read data through qradio protocol
         packet = qradio.read_data(whole_data)
         print(packet)
@@ -56,6 +77,7 @@ while True:
         # echo command
         print('sending data back to the client')
         connection.sendall('\xaa'.encode('latin-1'))
+        """
     finally:
         # Clean up the connection
         connection.close()
