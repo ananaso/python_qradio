d3e79bce32adecfba2c28098202886c3fe3ee741
Alden Davidson <adavidson+github@protonmail.ch>
2021-03-12 15:33:24 -0800

    read cosmos command header to know when whole msg received

--- socket_echo_server.py
+++ socket_echo_server.py
@@ -8,7 +8,7 @@ from qradio_protocol_cb import QradioProtocolCb
 
 # Get IP/Port from cmd args if available
 host = 'localhost'
-port = 51104
+port = 51103
 try:
     host = sys.argv[1]
     port = int(sys.argv[2])
@@ -40,21 +40,22 @@ while True:
 
         # Receive the data in small chunks
         whole_data = ''.encode('latin-1')
-        while True:
+        cmd_length = -1
+        while len(whole_data) != cmd_length:
             data = connection.recv(16)
-            print('received {!r}'.format(data))
-            if not data:
-                break
+            print(f"received {data!r}")
             whole_data += data
+            if cmd_length == -1 and len(whole_data) >= 8:
+                cmd_length = int.from_bytes(
+                    whole_data[4:8], byteorder='big', signed=False)
         
         # read data through qradio protocol
         packet = qradio.read_data(whole_data)
         print(packet)
-        
+            
         # echo command
-        if packet:
-            print('sending data back to the client')
-            connection.sendall(packet)
+        print('sending data back to the client')
+        connection.sendall('\xaa'.encode('latin-1'))
     finally:
         # Clean up the connection
         connection.close()
