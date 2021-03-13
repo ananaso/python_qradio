a33f4ac32cad107411262fd4f723c518c602e023
Alden Davidson <adavidson+github@protonmail.ch>
2021-03-12 16:11:30 -0800

    handle connection/disconnecting/reconnecting cosmos correctly

--- socket_echo_server.py
+++ socket_echo_server.py
@@ -35,38 +35,45 @@ sock.listen(1)
 while True:
     # Wait for a connection
     print('waiting for a connection')
+    
     connection, client_address = sock.accept()
     try:
         print('connection from', client_address)
-
-        # Receive the data in small chunks
-        whole_data = b''
-        cmd_length = -1
-        radiosync = b''
-        while len(whole_data) != cmd_length:
-            print(f"whole_data: {len(whole_data)} \ cmd_length: {cmd_length}")
-            data = connection.recv(16)
-            if data == b'':
-                break
-            print(f"received {data!r}")
-            whole_data += data
-            if cmd_length == -1 and len(whole_data) >= 8:
-                radiosync = whole_data[:4]
-                if radiosync == qradio.RADIOSYNC:
-                    cmd_length = struct.unpack(">I", whole_data[4:8])[0]
         
-        framesync = whole_data[16:20]
-        if framesync == qradio.FRAMESYNC:
-            cmd_counter = struct.unpack(">I", whole_data[8:12])[0]
-            cmd_data_bit_length = struct.unpack(">I", whole_data[12:16])[0]
-            data = whole_data[20:]
-            print("Unfurled received packet:")
-            print(f"\tRADIOSYNC: 0x{radiosync.hex()}")
-            print(f"\tcmd_length: {cmd_length}")
-            print(f"\tcmd_counter: {cmd_counter}")
-            print(f"\tcmd_data_bit_length: {cmd_data_bit_length}")
-            print(f"\tFRAMESYNC: 0x{framesync.hex()}")
-            print(f"\tdata: {data}")
+        stillConnected = True
+        while stillConnected:
+            whole_data = b''
+            cmd_length = -1
+            radiosync = b''
+            
+            # Receive the data in small chunks
+            while len(whole_data) != cmd_length:
+                data = connection.recv(16)
+                # if the connection to cosmos is closed we get empty data
+                if data == b'':
+                    # make sure to close the connection on our side too
+                    stillConnected = False
+                    break
+                print(f"received {data!r}")
+                whole_data += data
+                # determine command length if that part of header is received
+                if cmd_length == -1 and len(whole_data) >= 8:
+                    radiosync = whole_data[:4]
+                    if radiosync == qradio.RADIOSYNC:
+                        cmd_length = struct.unpack(">I", whole_data[4:8])[0]
+            
+            framesync = whole_data[16:20]
+            if framesync == qradio.FRAMESYNC:
+                cmd_counter = struct.unpack(">I", whole_data[8:12])[0]
+                cmd_data_bit_length = struct.unpack(">I", whole_data[12:16])[0]
+                data = whole_data[20:]
+                print("Unfurled received packet:")
+                print(f"\tRADIOSYNC: 0x{radiosync.hex()}")
+                print(f"\tcmd_length: {cmd_length}")
+                print(f"\tcmd_counter: {cmd_counter}")
+                print(f"\tcmd_data_bit_length: {cmd_data_bit_length}")
+                print(f"\tFRAMESYNC: 0x{framesync.hex()}")
+                print(f"\tdata: {data}")
         
         """
         REMOVED SENDBACK SINCE IT ISN'T WHAT COSMOS EXPECTS/WANTS
